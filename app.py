8c97a90d612a158df23fe2c82024a09a87152c4f
Samnoyal S <samnoyal33@gmail.com>
2024-10-28 00:13:18 +0530

    First Commit

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
 # Simulating an in-memory database
 todos = []
 
