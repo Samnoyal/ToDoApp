d986854fc3887ad76f26dc27db1adc32a9bf99d8
Samnoyal S <36767665+Samnoyal@users.noreply.github.com>
2024-10-28 17:29:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import os
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
 # Simulating an in-memory database
 todos = []
 
