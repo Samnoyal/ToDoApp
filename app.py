a327cd25e6abd2879b6ef034006e248ac2dc12a4
Samnoyal S <36767665+Samnoyal@users.noreply.github.com>
2024-10-28 17:43:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
-
+application = app
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
 
